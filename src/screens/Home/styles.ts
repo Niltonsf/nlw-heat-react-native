437cfe28fba3a7f5a344023eba8a498b07b97093
Niltonsf <nilton.schumacher.filho@gmail.com>
2021-10-21 19:47:25 -0300

    Added splash screen, themes, assets

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#121214',
+		flex: 1,
+		justifyContent: 'center', //vertical alignment
+		alignItems: 'center' // horizontal alignment
+	},
+	text: {
+		color: '#FFFFFF',
+	}
+});
+
+export default styles;
